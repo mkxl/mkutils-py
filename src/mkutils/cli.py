0069fe52bdd51d5e31b7c5f78908a7c06397c9bb
michael <66692526+mkxl@users.noreply.github.com>
2025-08-02 14:25:54 -0400

    updates

--- src/mkutils/cli.py
+++ src/mkutils/cli.py
@@ -10,7 +10,6 @@ from mkutils.utils import Utils
 
 @dataclasses.dataclass(kw_only=True)
 class Cli:
-    DEFAULT_ADD_DEFAULT_CALLBACK: ClassVar[bool] = True
     DEFAULT_PRETTY_EXCEPTIONS_ENABLE: ClassVar[bool] = False
 
     typer: Typer
@@ -19,13 +18,10 @@ class Cli:
     def new(
         cls,
         *,
-        add_default_callback: bool = DEFAULT_ADD_DEFAULT_CALLBACK,
         pretty_exceptions_enable: bool = DEFAULT_PRETTY_EXCEPTIONS_ENABLE,
     ) -> Self:
         typer = cls._typer(pretty_exceptions_enable=pretty_exceptions_enable)
         cli = cls(typer=typer)
-
-        cli._init(add_default_callback=add_default_callback)
 
         return cli
 
@@ -42,9 +38,8 @@ class Cli:
 
         return self
 
-    def _init(self, *, add_default_callback: bool) -> None:
-        if add_default_callback:
-            self.add_callback(fn=self._default_callback)
+    def add_default_callback(self) -> Self:
+        return self.add_callback(fn=self._default_callback)
 
     def add_command(self, *, name: Optional[str] = None, fn: Function[Any, Any]) -> Self:
         if inspect.iscoroutinefunction(fn):
