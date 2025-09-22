2d28cdbf56d4bba3c4cb5838c8e57cd59986ca05
michael <66692526+mkxl@users.noreply.github.com>
2025-09-21 22:15:34 -0400

    updates

--- src/mkutils/enum.py
+++ src/mkutils/enum.py
@@ -10,8 +10,8 @@ from pydantic_core.core_schema import CoreSchema
 # - this class serializes as a string and deserializes from a string using
 #   [enum_variant.string_repr() == str(enum_variant.value)]; this makes it compatible with typer which uses
 #   [str(enum_variant.value)] to (1) render the enum variant in the [--help] documentation and (2) to deserialize cli
-#   arguments back into the enum variant (see [generate_enum_convertor()] in
-#   [https://github.com/fastapi/typer/blob/master/typer/main.py])
+#   arguments back into the enum variant using a [str(enum_variant.value) -> enum_variant] dict (see
+#   [generate_enum_convertor()] in [https://github.com/fastapi/typer/blob/master/typer/main.py])
 class Enum(StdEnum):
     def string_repr(self) -> str:
         return str(self.value)
@@ -53,9 +53,11 @@ class Enum(StdEnum):
     #   [https://github.com/pydantic/pydantic-core/blob/main/python/pydantic_core/core_schema.py]
     @classmethod
     def __get_pydantic_core_schema__(cls, _source: type[Any], _handler: GetCoreSchemaHandler) -> CoreSchema:
+        expected = [enum_variant.string_repr() for enum_variant in cls]
+        json_schema_input_schema = pydantic_core.core_schema.literal_schema(expected)
         serialization = pydantic_core.core_schema.plain_serializer_function_ser_schema(function=cls.string_repr)
         schema = pydantic_core.core_schema.no_info_plain_validator_function(
-            function=cls._validator_fn, serialization=serialization
+            function=cls._validator_fn, json_schema_input_schema=json_schema_input_schema, serialization=serialization
         )
 
         return schema
